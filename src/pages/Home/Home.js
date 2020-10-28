2ecf6ee2634218f9b4e3a39f3acce704276ea6dd
jiradejDev <jiradej@sobtid.me>
2020-10-28 22:45:45 +0700

    feat: Carousel date

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import styles from "./Home.module.scss";
-import { Tabs } from "antd";
-import Slider from "react-slick";
-import GraphIcon from '../../assets/icons/graph_icon.svg'
-import GraphIconSelected from '../../assets/icons/graph_icon_selected.svg'
-import ListIcon from '../../assets/icons/list_icon.svg'
-import ListIconSelected from '../../assets/icons/list_icon_selected.svg'
+import { Tabs, Carousel } from "antd";
+import GraphIcon from "../../assets/icons/graph_icon.svg";
+import GraphIconSelected from "../../assets/icons/graph_icon_selected.svg";
+import ListIcon from "../../assets/icons/list_icon.svg";
+import ListIconSelected from "../../assets/icons/list_icon_selected.svg";
 
 const { TabPane } = Tabs;
 
 const Home = () => {
-  const [ viewMode, setViewMode ] = useState(0)
+  const [viewMode, setViewMode] = useState(0);
+  const [ carousel, setCarousel ] = useState(null)
   const callback = (key) => {
     console.log(key);
+  }; 
+  const onChangeCarousel = async(type) => {  
+    if(type === 'next') { 
+      await carousel.next() 
+    }else { 
+      await carousel.prev() 
+    } 
   } 
-  const settings = {
-    className: "center",
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 5,
-    swipeToSlide: true,
-    afterChange: function(index) {
-      console.log(
-        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
-      );
-    }
-  };
   return (
     <div className={styles.home}>
       <div className={styles.home_menu}>
@@ -36,53 +31,96 @@ const Home = () => {
       <div className={styles.divided} />
       <div className={styles.date_container}>
         <div className={styles.menu}>
-          <Tabs defaultActiveKey="1" onChange={callback} >
-            <TabPane tab="Daily" key="1"/>
-            <TabPane tab="Weekly" key="2"/>
-            <TabPane tab="Monthly" key="3"/>
+          <Tabs defaultActiveKey="1" onChange={callback}>
+            <TabPane tab="Daily" key="1" />
+            <TabPane tab="Weekly" key="2" />
+            <TabPane tab="Monthly" key="3" />
           </Tabs>
           <div onClick={() => setViewMode(0)} className={styles.icon}>
-              <img src={viewMode === 0 ? GraphIconSelected : GraphIcon} alt='GraphIcon'/>
-            </div>
-            <div onClick={() => setViewMode(1)} className={styles.icon}>
-              <img src={viewMode === 1 ? ListIconSelected : ListIcon} alt='ListIcon'/>
-            </div>
+            <img
+              src={viewMode === 0 ? GraphIconSelected : GraphIcon}
+              alt="GraphIcon"
+            />
+          </div>
+          <div onClick={() => setViewMode(1)} className={styles.icon}>
+            <img
+              src={viewMode === 1 ? ListIconSelected : ListIcon}
+              alt="ListIcon"
+            />
+          </div>
         </div>
         <div className={styles.date}>
-        <div >
-        {/* <Slider {...settings}>
-          <div>
-            <h3>1</h3>
+          <div className={styles.date_div}>
+            <div onClick={() => onChangeCarousel('prev')} className={styles.date_item_arrow}>
+              <span>{'<'}</span>
+              {/* <img src={LeftArrowIcon} alt="LeftArrowIcon" /> */}
+              {/* <span>←</span> */}
+            </div>
+            <div className={styles.carousel_container}>
+              <Carousel
+                ref={node => setCarousel(node)} 
+                // arrows={true}
+                slidesToShow={7}
+                swipeToSlide={true}
+                dots={false}
+                draggable={true}
+                infinite={false}
+                afterChange={onChangeCarousel}
+                // nextArrow={<div className={styles.arrow}>→</div>}
+                // prevArrow={<div className={styles.arrow}>←</div>}
+              >
+                {[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16,
+                  17,
+                  18,
+                  19,
+                  20,
+                  21,
+                  22,
+                  23,
+                  24,
+                  25,
+                  26,
+                  27,
+                  28,
+                  29,
+                  30,
+                ].map((data, i) => {
+                  return (
+                    <div key={i}>
+                      <div className={styles.date_item}>
+                        <span>{data}</span>
+                      </div>
+                    </div>
+                  );
+                })}
+                <div></div>
+              </Carousel>
+            </div>
+            <div onClick={() => onChangeCarousel('next')} className={styles.date_item_arrow}>
+              <span>{'>'}</span>
+              {/* <img src={RightArrowIcon} alt="RightArrowIcon" /> */}
+              {/* <span>→</span> */}
+            </div>
+            <div className={styles.text_div}>
+              <span className={styles.text_today}>↓Today</span>
+            </div>
           </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
-          </div>
-        </Slider> */}
-        </div>
-        <div className={styles.text_div}>
-          <span className={styles.text_today}>Today</span>
-        </div>
         </div>
       </div>
     </div>
